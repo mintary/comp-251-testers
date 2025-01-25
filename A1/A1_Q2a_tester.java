a798a2802166f18d42875b63149c596a936b1c70
mintary <ewang0332@gmail.com>
2025-01-25 18:55:12 -0500

    a1 q2a should return merged set rep

--- A1/A1_Q2a_tester.java
+++ A1/A1_Q2a_tester.java
@@ -30,45 +30,80 @@ public class A1_Q2a_tester {
     @Test
     public void testUnion() {
         DisjointSetsA ds = new DisjointSetsA(10);
-        int rankUnion = ds.union(0, 1);
+        int unionRoot = ds.union(0, 1);
         int root0 = ds.find(0);
         int root1 = ds.find(1);
-        assertEquals(rankUnion, 1);
+        assertEquals(unionRoot, 1);
         assertTrue(root0 == 0 || root0 == 1);
         assertTrue(root1 == 0 || root1 == 1);
         assertEquals(root0, root1);
     }
+
 
     @Test
     public void testUnionDifferentRanks() {
         DisjointSetsA ds = new DisjointSetsA(10);
-        int rankUnion = ds.union(0, 1);
+        int unionRoot = ds.union(0, 1);
         int root0 = ds.find(0);
         int root1 = ds.find(1);
-        assertEquals(rankUnion, 1);
+        assertEquals(unionRoot, 1);
         assertTrue(root0 == 0 || root0 == 1);
         assertTrue(root1 == 0 || root1 == 1);
         assertEquals(root0, root1);
 
-        int rankUnionDifferent = ds.union(1, 2);
+        int unionRootDifferent = ds.union(1, 2);
         int root2 = ds.find(2);
-        assertTrue(root2 == 1); 
-        assertEquals(rankUnionDifferent, 1);
+        assertEquals(unionRootDifferent, 1);
+        assertEquals(root2, 1);
     }
-
 
     @Test
     public void testUnionTwice() {
         DisjointSetsA ds = new DisjointSetsA(10);
-        int rankFirstUnion = ds.union(0, 1);
+        int unionRootFirst = ds.union(0, 1);
         boolean firstUnion = ds.find(0) == ds.find(1);
-        int rankSecondUnion = ds.union(0, 1);
+        int unionRootSecond = ds.union(0, 1);
         boolean secondUnion = ds.find(0) == ds.find(1);
-        assertEquals(rankFirstUnion, 1);
-        assertEquals(rankSecondUnion, 1);
+        assertEquals(unionRootFirst, 1);
+        assertEquals(unionRootSecond, 1);
         assertTrue(firstUnion);
         assertTrue(secondUnion);
     }
 
+    @Test
+    public void testUnionWithPathCompression() {
+        DisjointSetsA ds = new DisjointSetsA(10);
+        ds.union(0, 1);
+        ds.union(1, 2);
+        ds.union(2, 3);
+        ds.union(3, 4);
+        ds.union(4, 5);
+        ds.union(5, 6);
+        ds.union(6, 7);
+        ds.union(7, 8);
+        ds.union(8, 9);
+
+        for (int i = 0; i < 10; i++) {
+            assertEquals(ds.find(0), ds.find(i));
+        }
+    }
+
+    @Test
+    public void testUnionWithEqualRanks() {
+        DisjointSetsA ds = new DisjointSetsA(10);
+        ds.union(0, 1);
+        ds.union(2, 3);
+        ds.union(4, 5);
+        ds.union(6, 7);
+        ds.union(8, 9);
+        ds.union(0, 2);
+        ds.union(4, 6);
+        ds.union(8, 0);
+        ds.union(4, 8);
+
+        for (int i = 0; i < 10; i++) {
+            assertEquals(ds.find(0), ds.find(i));
+        }
+    }
 
 }
